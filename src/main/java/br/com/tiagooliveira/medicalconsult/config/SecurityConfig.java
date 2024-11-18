922503f4ab0b689f921fe3b1ae262ad16e90e0d3
Tiago Lelis <130498904+Lelis19@users.noreply.github.com>
2024-11-18 20:08:12 -0300

    Eighth Commit
    Pequena modificação em SecurityConfig

--- src/main/java/br/com/tiagooliveira/medicalconsult/config/SecurityConfig.java
+++ src/main/java/br/com/tiagooliveira/medicalconsult/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,17 +17,39 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private static final String[] PUBLIC_MATCHERS ={
+            "/h2-console/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    };
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/usuarios",
+            "/consultas"
+    };
+    private static final String[] PUBLIC_MATCHERS_PUT = {
+            "/usuarios/**",
+            "/consultas/**"
+    };
+    private static final String[] PUBLIC_MATCHERS_GET = {
+            "/usuarios/**",
+            "/consultas/**"
+    };
+    private static final String[] PUBLIC_MATCHERS_DELETE = {
+            "/usuarios/**",
+            "/consultas/**"
+    };
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.csrf(csrf -> csrf.disable())
+                .headers(h -> h.frameOptions().disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/usuario")
+                        .requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST)
                         .hasAnyRole("ADMIN", "SECRETARIO")
-                        .requestMatchers(HttpMethod.PUT, "/usuario/**")
+                        .requestMatchers(HttpMethod.PUT, PUBLIC_MATCHERS_PUT)
                         .hasAnyRole("ADMIN", "SECRETARIO")
-                        .requestMatchers(HttpMethod.DELETE, "/usuario/**")
+                        .requestMatchers(HttpMethod.DELETE, PUBLIC_MATCHERS_DELETE)
                         .hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/usuarios/**")
+                        .requestMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET)
                         .permitAll()
                         .anyRequest().authenticated()
                 )
