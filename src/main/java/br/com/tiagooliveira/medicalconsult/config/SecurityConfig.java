164fb3b2b0f38bd87749c2daf6296b5a966691d7
Tiago Lelis <130498904+Lelis19@users.noreply.github.com>
2024-11-18 20:48:43 -0300

    Update SecurityConfig.java

--- src/main/java/br/com/tiagooliveira/medicalconsult/config/SecurityConfig.java
+++ src/main/java/br/com/tiagooliveira/medicalconsult/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.csrf(csrf -> csrf.disable())
                 .headers(h -> h.frameOptions().disable())
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST)
                         .hasAnyRole("ADMIN", "SECRETARIO")
