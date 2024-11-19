fbedba59391cbf8089abdec4572598418a2f3ed5
Tiago Lelis <130498904+Lelis19@users.noreply.github.com>
2024-11-18 21:00:01 -0300

    Upgrade SecurityConfig

--- src/main/java/br/com/tiagooliveira/medicalconsult/config/SecurityConfig.java
+++ src/main/java/br/com/tiagooliveira/medicalconsult/config/SecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -67,5 +72,16 @@ public class SecurityConfig {
                 .withUser("Beltrano").password(passwordEncoder.encode("123456")).roles("SECRETARIO")
                 .and()
                 .withUser("Sicrano").password(passwordEncoder.encode("123456")).roles("PACIENTE");
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/*", configuration);
+        return source;
     }
 }
